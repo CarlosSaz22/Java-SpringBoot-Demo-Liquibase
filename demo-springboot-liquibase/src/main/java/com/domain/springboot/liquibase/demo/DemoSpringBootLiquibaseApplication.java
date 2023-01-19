81e41dc25c7949f082b6a9b01c94faf5665c707e
CarlosSaz22 <61812538+CarlosSaz22@users.noreply.github.com>
2023-01-19 15:44:11 -0600

    Agregar proyecto demo

---
+++ demo-springboot-liquibase/src/main/java/com/domain/springboot/liquibase/demo/DemoSpringBootLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.domain.springboot.liquibase.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootLiquibaseApplication.class, args);
+	}
+
+}
---
+++ demo-springboot-liquibase/src/test/java/com/domain/springboot/liquibase/demo/DemoSpringBootLiquibaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domain.springboot.liquibase.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootLiquibaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
